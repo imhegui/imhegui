96b49b61ed42b79f00c9a98d962a382deac82f64
imhegui <hegui3555@gmail.com>
2024-07-22 17:41:21 +0800

    add new blog

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1720702515517";
+const VERSION = "1721641275616";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
